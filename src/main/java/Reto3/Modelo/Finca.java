d31b9755a10c76de3420b2b20a0a479ff5baab61
felixcesp <jose.cespedes.mt@correo.usa.edu.co>
2021-11-02 08:47:16 -0500

    buenas practicas

--- src/main/java/Reto3/Modelo/Finca.java
+++ src/main/java/Reto3/Modelo/Finca.java
@@ -13,91 +13,175 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+/**
+ * representa una finca
+ * @author FELIX CESPEDES
+ */
 @Entity
 @Table(name = "farm")
 public class Finca implements Serializable {
-
+    /**
+     * anotaciones id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+     * identificador de finca
+     */
     private Integer id;
+    /**
+     * nombre finca
+     */
     private String name;
+    /**
+     * direccion finca
+     */
     private String address;
+    /**
+     * extencion finca
+     */
     private Integer extension;
+    /**
+     * descripcion finca
+     */
     private String description;
-
+    /**
+     * relaciones
+     */
     @ManyToOne
     @JoinColumn(name = "categoryid")
     @JsonIgnoreProperties("farms")
+    /**
+     * categoria finca
+     */
     private Categoria category;
-
+    /**
+     * relaciones
+     */
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
     @JsonIgnoreProperties({"farm", "client"})
+    /**
+     * listado mensajes finca
+     */
     private List<Mensaje> messages;
-
+    /**
+     * relaciones
+     */
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
     @JsonIgnoreProperties({"farm", "message"})
+    /**
+     * listado reservaciones
+     */
     public List<Reservacion> reservations;
-
+    /**
+     * identificacion
+     * @return 
+     */
     public Integer getId() {
         return id;
     }
-
+    /**
+     * identificacion
+     * @param id 
+     */
     public void setId(Integer id) {
         this.id = id;
     }
-
+    /**
+     * nombre
+     * @return 
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * nombre
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * direccion
+     * @return 
+     */
     public String getAddress() {
         return address;
     }
-
+    /**
+     * direccion
+     * @param address 
+     */
     public void setAddress(String address) {
         this.address = address;
     }
-
+    /**
+     * extension
+     * @return 
+     */
     public Integer getExtension() {
         return extension;
     }
-
+   /**
+    * descripcion
+    * @param extension 
+    */
     public void setExtension(Integer extension) {
         this.extension = extension;
     }
-
+    /**
+     * descripcion
+     * @return 
+     */
     public String getDescription() {
         return description;
     }
-
+    /**descripcion
+     * 
+     * @param description 
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /**
+     * categoria
+     * @return 
+     */
     public Categoria getCategory() {
         return category;
     }
-
+    /**
+     * categoria
+     * @param category 
+     */
     public void setCategory(Categoria category) {
         this.category = category;
     }
-
+    /**
+     * mensajes
+     * @return 
+     */
     public List<Mensaje> getMessages() {
         return messages;
     }
-
+    /**
+     * mensajes
+     * @param messages 
+     */
     public void setMessages(List<Mensaje> messages) {
         this.messages = messages;
     }
-
+    /**
+     * reservaciones
+     * @return 
+     */
     public List<Reservacion> getReservations() {
         return reservations;
     }
-
+    /**
+     * reservaciones
+     * @param reservations 
+     */
     public void setReservations(List<Reservacion> reservations) {
         this.reservations = reservations;
     }
--- src/main/java/Reto3/Modelo/Reservacion.java
+++ src/main/java/Reto3/Modelo/Reservacion.java
@@ -10,84 +10,155 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+/**
+ * representa una reservacion
+ * @author FELIX CESPEDES
+ */
 @Entity
 @Table(name = "reservation")
 public class Reservacion implements Serializable {
-
+    /**
+     * anotaciones id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+     * identificador reservacion
+     */
     private Integer idReservation;
+    /**
+     * fecha inicio
+     */
     private Date startDate;
+    /*
+    fecha devolucio
+    */
     private Date devolutionDate;
+    /**
+     * estado
+     */
     private String status = "created";
-
+    /**
+     * relaciones
+     */
     @ManyToOne
     @JoinColumn(name = "id")
     @JsonIgnoreProperties("reservations")
+    /**
+     * identificador finca
+     */
     private Finca farm;
-
+    /**
+     * relaciones
+     */
     @ManyToOne
     @JoinColumn(name = "idCliente")
     @JsonIgnoreProperties({"reservations", "messages"})
+    /**
+     * identificador cliente
+     */
     private Cliente client;
-
+    /**
+     * identificador score
+     */
     private String score;
-
+    /**
+     * reservacion
+     * @return 
+     */
     public Integer getIdReservation() {
         return idReservation;
     }
-
+    /**
+     * reservacion
+     * @param idReservation 
+     */
     public void setIdReservation(Integer idReservation) {
         this.idReservation = idReservation;
     }
-
+    /**
+     * fecha inicio
+     * @return 
+     */
     public Date getStartDate() {
         return startDate;
     }
-
+    /**
+     * fecha inicio
+     * @param startDate 
+     */
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
-
+    /**
+     * fecha devolucion
+     * @return 
+     */
     public Date getDevolutionDate() {
         return devolutionDate;
     }
-
+    /**
+     * fecha devolucion
+     * @param devolutionDate 
+     */
     public void setDevolutionDate(Date devolutionDate) {
         this.devolutionDate = devolutionDate;
     }
-
+    /**
+     * estado
+     * @return 
+     */
     public String getStatus() {
         return status;
     }
-
+    /**
+     * estado
+     * @param status 
+     */
     public void setStatus(String status) {
         this.status = status;
     }
-
+    /**
+     * finca
+     * @return 
+     */
     public Finca getFarm() {
         return farm;
     }
-
+    /**
+     * finca
+     * @param farm 
+     */
     public void setFarm(Finca farm) {
         this.farm = farm;
     }
-
+    /**
+     * cliente
+     * @return 
+     */
     public Cliente getClient() {
         return client;
     }
-
+    /**
+     * cliente
+     * @param client 
+     */
     public void setClient(Cliente client) {
         this.client = client;
     }
-
+    /**
+     * score
+     * @return 
+     */
     public String getScore() {
         return score;
     }
-
+    /**
+     * score
+     * @param score 
+     */
     public void setScore(String score) {
         this.score = score;
     }
-
+    
 }
